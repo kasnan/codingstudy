165bee2a74882ad49f73ef2c2617968cd502edc3
Kimdohyeon <kasnanganji@gmail.com>
2023-08-31 15:54:15 +0900

    002 update

--- 002.cpp
+++ 002.cpp
@@ -8,22 +8,19 @@ int main(){
     cin >> N;
 
     vector<float> v1;
-    float tmp;
+    float tmp, max=-1;
     float sum=0;
 
     for(int i=0;i<N;i++){
         cin >> tmp;
+        if(tmp > max){
+            max = tmp;
+        }
         v1.push_back(tmp);
     }
 
-    tmp = v1.front();
     for(int i=0;i<N;i++){
-        if(v1[i]>tmp)
-            tmp=v1[i];
-    }
-
-    for(int i=0;i<N;i++){
-        v1[i] = (v1[i]/tmp)*100;
+        v1[i] = (v1[i]/max)*100;
         sum+=v1[i];
     }
     sum/=N;
