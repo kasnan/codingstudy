467ee092e29d72d2dcccda8bb901c5fbeff413e1
Kimdohyeon <kasnanganji@gmail.com>
2023-09-01 18:10:38 +0900

    09901 finish

--- 005.cpp
+++ 005.cpp
@@ -14,13 +14,11 @@ int main(){
     vector<long> S(N,0);
     vector<long> C(M,0);
     cin >> S[0];
-
     for(int i=1;i<N;i++){
         long tmp;
         cin >> tmp;
         S[i] = S[i-1] + tmp;
     }
-
     for(int i=0;i<N;i++){
         int remainder = S[i]%M;
         if(remainder == 0){
@@ -28,14 +26,10 @@ int main(){
         }
         C[remainder]++;
     }
-
     for(int i=0;i<M;i++){
         if(C[i]>1){
             count+=(C[i]*(C[i]-1)/2);
         }
     }
-
     cout << count << endl;
-
-
 }
