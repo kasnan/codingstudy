b844097237f8d64be3beb247866f133befffc881
Kimdohyeon <kasnanganji@gmail.com>
2023-09-12 20:10:54 +0900

    031 solved

--- 031.cpp
+++ 031.cpp
@@ -1,27 +1,39 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
 
 using namespace std;
 
 int main(){
-    int N,K;
+    long N,K;
     cin >> N;
-
     cin >> K;
 
-    vector<vector<int>>A;
-    A.resize(N+1);
+    long s=1;
+    long e=K;
+    long ans=0;
+    while(s<=e){
+        long mid = (s+e)/2;
+        long cnt = 0;
 
-    for(int i=1;i<N+1;i++){
-        for(int j=1;j<N+1;j++){
-            A[i].push_back(i*j);
+        // 중앙값보다 작은 수의 개수를 행마다 탐색
+        // 이 때 각 행의 수는 i*1 ~ i*N이다.
+        // 각 행마다 브루트포스적인 접근 방법이 아니더라도
+        // 중앙값에 각 행의 idx를 나눈 값을 N과 비교해서 구할 수 있다.
+        for(int i=1;i<N+1;i++){
+            cnt+=min(mid/i,N);
+        }
+        // 중앙값보다 작거나 같은 수가 K보다 적으면 더 큰 범위로 탐색
+        if(cnt < K){
+            s = mid + 1;
+        }
+        // K이상이면 일단 현재 단계의 탐색값을 저장하고 더 작은 범위로 탐색
+        // 이는 더 작은 범위로 줄이기 위해 e 인덱스를 mid-1로 줄이고 나서
+        // while문 탈출 조건이 완료되면 최종 단계에 해당
+        else{
+            ans=mid;
+            e = mid - 1;
         }
     }
 
-    vector<int>B(N+1);
-
-
-
-    cout << B[k] << endl;
+    cout << ans << endl;
 }
