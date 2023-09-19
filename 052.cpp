1cd9770c993d94fb40e1d7ffc69c979046f7b4fc
Kimdohyeon <kasnanganji@gmail.com>
2023-09-19 23:02:40 +0900

    052 solving

---
+++ 052.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int findfunc(int n);
+void unionfunc(int a,int b);
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n,m;
+    cin >> n >> m;
+
+    int truthp;
+    cin >> truthp;
+
+    for(int i=0;i<truthp;i++){
+        int pnum;
+        cin >> pnum;
+    }
+}
