afcacde9aadf102094a4784a860ee125798e2467
Kimdohyeon <kasnanganji@gmail.com>
2023-09-27 18:16:11 +0900

    072 solved

--- 072.cpp
+++ 072.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 #include<cmath>
 #include<vector>
+#include<limits.h>
 using namespace std;
 
-vector<int> segtree;
-
-int getMin(int s,int e);
+vector<long> segtree;
+long getMin(int s,int e);
 void setTree(int i);
 
 int main(){
@@ -18,6 +18,7 @@ int main(){
 
     int treeHeight=0;
     int Length=n;
+
     while(Length!=0){
         Length/=2;
         treeHeight++;
@@ -26,48 +27,48 @@ int main(){
     int treeSize=int(pow(2,treeHeight+1));
     int startIndex=treeSize/2-1;
     segtree.resize(treeSize+1);
+    fill(segtree.begin(),segtree.end(),LONG_MAX);
 
     // 데이터 입력(시작 위치부터 데이터 입력)
     for(int i=startIndex+1;i<startIndex+n+1;i++){
         cin >> segtree[i];
     }
-    
-    // 입력된 데이터 상위 노드들 데이터 연산
-    // 구간 합이니 두 자식 노드의 합이 부모 노드의 값
     setTree(treeSize-1);
 
     for(int i=0;i<m;i++){
-        int a,b;
+        long a,b;
         cin >> a >> b;
 
         a+=startIndex;
         b+=startIndex;
 
-        cout << getMin(a,b) << endl;
+        cout << getMin(a,b) << "\n";
     }
 }
 
 void setTree(int i){
     while(i!=1){
-        segtree[i/2]=min(segtree[i/2*2],segtree[i/2*2+1]);
+        if(segtree[i/2]>segtree[i]){
+            segtree[i/2]=segtree[i];
+        }
         i--;
     }
 }
-int getSum(int s,int e){
-    int partSum=0;
+long getMin(int s,int e){
+    long minv=LONG_MAX;
 
     while(s<=e){
         if(s%2==1){
-            partSum+=segtree[s];
+            minv=min(minv,segtree[s]);
             s++;
         }
         if(e%2==0){
-            partSum+=segtree[e];
+            minv=min(minv,segtree[e]);
             e--;
         }
         s/=2;
         e/=2;
     }
 
-    return partSum;
+    return minv;
 }
