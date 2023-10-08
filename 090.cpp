81ed9f24ca4c7d8b35fd9ee08b82c439ba7dea87
kasnan <kasnanganji@gmail.com>
2023-10-08 14:10:43 +0900

    090 solved

--- 090.cpp
+++ 090.cpp
@@ -1,36 +1,18 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
 
-int D[1000][1000];
-
+int D[1001][1001];
+string s1,s2;
+vector<char> res;
+void getText(int i,int j);
 int main(){
-    string s1,s2;
+    
     cin >> s1 >> s2;
 
-    int col,row;
-    col=s1.length();
-    row=s2.length();
-
-
-    // D테이블 초기화
-    bool flag=false;
-    for(int i=0;i<col;i++){
-        if(flag){
-            D[i][0]=1;
-        }
-        else if(s1[i]==s2[0]){
-            D[i][0]=1;
-            flag=true;
-        }
-        else{
-            D[i][0]=0;
-        }
-    }
-
-    for(int i=1;i<col;i++){
-        for(int j=1;j<row;j++){
-            if(s1[i]==s2[j]){
+    for(int i=1;i<s1.size()+1;i++){
+        for(int j=1;j<s2.size()+1;j++){
+            if(s1[i-1]==s2[j-1]){
                 D[i][j]=D[i-1][j-1]+1;
             }
             else{
@@ -38,5 +20,32 @@ int main(){
             }
         }
     }
-    
+    cout << D[s1.size()][s2.size()] << "\n";
+    if(D[s1.size()][s2.size()]){
+        // LCS 출력
+        // 테이블 왼쪽과 위쪽중 같은 값을 선택
+        getText(s1.size(),s2.size());
+        for(int i=res.size()-1;i>-1;i--){
+            cout << res[i];
+        }
+    }
+}
+
+void getText(int i,int j){
+    if(i==0 || j==0){
+        return;
+    }
+
+    if(s1[i-1]==s2[j-1]){
+        res.push_back(s1[i-1]);
+        getText(i-1,j-1);
+    }
+    else {
+        if(D[i-1][j]>D[i][j-1]){
+            getText(i-1,j);
+        }
+        else{
+            getText(i,j-1);
+        }
+    }
 }
---
+++ 091.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int D[1000][1000];
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+
+    for(int i=0;i<n;i++){
+        string line;
+        cin >> line;
+        for(int j=0;j<m;j++){
+
+        }
+    }
+}
