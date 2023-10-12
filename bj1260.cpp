f86df6951094bdd605397df495992f1ad6d0bac9
Kimdohyeon <kasnanganji@gmail.com>
2023-10-12 21:21:45 +0900

    1260 solved

--- bj1260.cpp
+++ bj1260.cpp
@@ -1,66 +1,67 @@
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
 using namespace std;
-#define MAX 1001
- 
-int N, M, V; //정점개수, 간선개수, 시작정점
-int map[MAX][MAX]; //인접 행렬 그래프
-bool visited[MAX]; //정점 방문 여부
-queue<int> q;
- 
-void reset() {
-    for (int i = 1; i <= N; i++) {
-        visited[i] = 0;
+
+vector<bool> visit;
+vector<vector<int>> A;
+
+void DFS(int s);
+void BFS(int s);
+
+int main(){
+    int n,e,s;
+    cin >> n >> e >> s;
+    A.resize(n+1);
+    visit.resize(n+1);
+
+    for(int i=0;i<e;i++){
+        int a,b;
+        cin >> a >> b;
+        A[a].push_back(b);
+        A[b].push_back(a);
     }
+
+    for(int i=1;i<n+1;i++){
+        sort(A[i].begin(),A[i].end());
+    }
+
+    fill(visit.begin(),visit.end(),false);
+    cout << s << " ";
+    DFS(s);
+    cout << "\n";
+    fill(visit.begin(),visit.end(),false);
+    cout << s << " ";
+    BFS(s);
 }
- 
-void DFS(int v) {
-    visited[v] = true;
-    cout << v << " ";
+
+void DFS(int s){
+    visit[s]=true;
     
-    for (int i = 1; i <= N; i++) {
-        if (map[v][i] == 1 && visited[i] == 0) { //현재 정점과 연결되어있고 방문되지 않았으면
+    for(int i:A[s]){
+        if(!visit[i]){
+            visit[i]=true;
+            cout << i << " ";
             DFS(i);
         }
     }
 }
- 
-void BFS(int v) {
-    q.push(v);
-    visited[v] = true;
-    cout << v << " ";
- 
-    while (!q.empty()) {
-        v = q.front();
-        q.pop();
-        
-        for (int w = 1; w <= N; w++) {
-            if (map[v][w] == 1 && visited[w] == 0) { //현재 정점과 연결되어있고 방문되지 않았으면
-                q.push(w);
-                visited[w] = true;
-                cout << w << " ";
+
+void BFS(int s){
+    queue<int> q1;
+    visit[s]=true;
+    q1.push(s);
+    
+    while(!q1.empty()){
+        int now=q1.front();
+        q1.pop();
+        for(int next : A[now]){
+            if(!visit[next]){
+                cout << next << " ";
+                visit[next]=true;
+                q1.push(next);
             }
         }
     }
-}
- 
-int main() {
-    cin >> N >> M >> V;
- 
-    for (int i = 0; i < M; i++) {
-        int a, b;
-        cin >> a >> b;
-        map[a][b] = 1;
-        map[b][a] = 1;
-    }
- 
-    reset();
-    DFS(V);
-    
-    cout << '\n';
-    
-    reset();
-    BFS(V);
- 
-    return 0;
 }
