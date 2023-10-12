40bbacf1e4dfeeb56431104591bb2a941af436bb
Kimdohyeon <kasnanganji@gmail.com>
2023-10-13 05:04:28 +0900

    2667 solving

--- bj2667.cpp
+++ bj2667.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<queue>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int MAP[26][26];
@@ -7,24 +9,69 @@ bool visit[26][26];
 int dx[]={1,-1,0,0};
 int dy[]={0,0,1,-1};
 
+int n;
 int apt_group=0;
+vector<int> grp;
 
-void BFS(int y,int x);
+
+int BFS(int y,int x);
 
 int main(){
-    int n;
     cin >> n;
     for(int i=1;i<n+1;i++){
         string line;
         cin >> line;
         for(int j=1;j<n+1;j++){
-            MAP[i][j]=line[j-1]-'0';
+            MAP[i][j]=line[j-1]-'0';       
         }
     }
 
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(!visit[i][j] && MAP[i][j]!=0){
+                grp.push_back(BFS(i,j));
+                apt_group++;
+            }
+        }
+    }
+
+    sort(grp.begin(),grp.end());
+    cout << apt_group << endl;
+    for(int i=0;i<grp.size();i++){
+        cout << grp[i] << endl;
+    }
+
+    
+
 }
 
-void BFS(int y,int x){
+int BFS(int y,int x){
+    int cnt=0;
     queue<pair<int,int>> q1;
-    
+    visit[y][x]=true;
+    q1.push(make_pair(y,x));
+
+    while(!q1.empty()){
+        pair<int,int> now=q1.front();
+        q1.pop();
+        cnt++;
+        int nowy=now.first;
+        int nowx=now.second;
+        MAP[nowy][nowx]+=apt_group;
+
+        for(int i=0;i<4;i++){
+            int nexty=nowy+dy[i];
+            int nextx=nowx+dx[i];
+
+            if(0<nexty<n+1 && 0<nextx<n+1){
+                if(!visit[nexty][nextx] && MAP[nexty][nextx]!=0){
+                    visit[nexty][nextx]=true;
+                    q1.push(make_pair(nexty,nextx));
+                }
+            }
+
+        }
+    }
+
+    return cnt;
 }
