65aaa1725581eeeaf9c64826ce4d33e6dc84da46
Kimdohyeon <kasnanganji@gmail.com>
2023-10-19 21:36:57 +0900

    1309solving

---
+++ bj1309.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#define MOD 9901;
+using namespace std;
+
+int A[100000][2];
+int D[100000][2];
+int n;
+
+int main(){
+    cin >> n;
+    for(int i=0;i<n;i++){
+        D[i][]
+    }
+}
