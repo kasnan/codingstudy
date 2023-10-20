5b4ceb88a04968e65db6dc49c40adf69579c52ce
Kimdohyeon <kasnanganji@gmail.com>
2023-10-20 12:06:52 +0900

    1309 solved

--- bj1309.cpp
+++ bj1309.cpp
@@ -3,12 +3,23 @@
 using namespace std;
 
 int A[100000][2];
-int D[100000][2];
+int D[100000][3];
 int n;
 
 int main(){
     cin >> n;
-    for(int i=0;i<n;i++){
-        D[i][]
+    D[1][0]=1;
+    D[1][1]=1;
+    D[1][2]=1;
+
+    for(int i=2;i<n+1;i++){
+        D[i][0]=(D[i-1][1]+D[i-1][2])%MOD;
+        D[i][1]=(D[i-1][0]+D[i-1][2])%MOD;
+        D[i][2]=(D[i-1][0]+D[i-1][1]+D[i-1][2])%MOD;
     }
+    int sum=0;
+    for(int i=0;i<3;i++){
+        sum+=D[n][i];
+    }
+    cout << sum % MOD;
 }
